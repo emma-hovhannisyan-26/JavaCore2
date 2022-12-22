377ff9526e0bd49bf236f16af235688e27222192
emma-hovhannisyan <emmahovhannisyan555@gmail.com>
2022-12-22 10:37:02 +0400

    Домашняя работа, измененный

---
+++ src/homework1/Example1.java
@@ -0,0 +1,14 @@
+package homework1;
+
+public class Example1 {
+    public static void main(String[] args) {
+        int a = 18;
+        int b = 22;
+
+        if (a == b) {
+            System.out.println("a-ն հավասար է բ-ին");
+        } else {
+            System.out.println("a-ն հավասար չէ b-ին");
+        }
+    }
+}
--- src/homework1/Figure3.java
+++ src/homework1/Figure3.java
@@ -4,10 +4,13 @@ public class Figure3 {
     public static void main(String[] args) {
 
         for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < i; j++) {
-                System.out.print("*");
+            for (int j = 4; j > i; j--) {
+                System.out.print("  ");
             }
-            System.out.println("\n");
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
         }
     }
 }
